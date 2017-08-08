43999c25f8c6f623eb743da32392510dd3461a6b
siddhesh1996 <ksiddhesh96.sk@gmail.com>
2017-08-09 01:48:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -87,6 +87,10 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
 app.get('/:articleName', function (req,res){
     //articleName=Article-one
     //article[articleName]={} content object for article one
