107cbb2fb80273b79c5da5bcbc2edbb477cd450b
siddhesh1996 <ksiddhesh96.sk@gmail.com>
2017-08-09 00:48:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -87,13 +87,6 @@ app.get('/:articleName', function (req,res){
    res.send(createtemplate(articles[articleName]));
 });
 
-app.get('/article-two', function (req,res){
-   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
-app.get('/article-three', function (req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-});
-
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
