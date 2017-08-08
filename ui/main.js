c2a7dc14b9c6bfce799417afb71f108d78a39e7e
siddhesh1996 <ksiddhesh96.sk@gmail.com>
2017-08-09 02:21:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -12,9 +12,14 @@ function moveRight(){
     
     marginLeft=marginLeft + 1;
     img.style.marginLeft= marginLeft + 'px';
+    element.style.marginLeft = marginLeft + 'px';
 }
 img.onclick = function(){
    var interval = setInterval(moveRight, 50);
   
     
+};
+
+element.onclick =function(){
+    var interval1 = setInterval(moveRight, 50);
 };
