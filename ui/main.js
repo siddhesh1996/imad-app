b8966c5b19cc2c17e47d796ab6e6ae9c934359f7
siddhesh1996 <ksiddhesh96.sk@gmail.com>
2017-08-09 02:10:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,3 +4,10 @@ console.log('Loaded!');
 
 var element = document.getElementById('main-text');
 element.innerHTML ='New value';
+
+
+var img= document.getElementById('sid');
+img.onclick = function(){
+  img.style.marginLeft = '100px';  
+    
+};
