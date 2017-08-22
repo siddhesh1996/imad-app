3cb075311387f1513e226423a4a0ecab8adacbf8
Siddhesh Kasar <ksiddhesh96.sk@gmail.com>
2017-08-23 00:00:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,25 +1,19 @@
-console.log('Loaded!');
+//counter
+ var button = document.getElemnetById("counter");
+ var counter = 0;
+ button.onclick = function() {
+     //make a request to the counter endpoint
+     
+     
+     // capture the response and store it in variable
+     
+     
+     //render the variable in correct span
 
-//To change the text in html
-
-var element = document.getElementById('main-text');
-element.innerHTML ='Welcome Click Me';
-
-
-var img= document.getElementById('sid');
-var marginLeft=0;
-function moveRight(){
-    
-    marginLeft=marginLeft + 1;
-    img.style.marginLeft= marginLeft + 'px';
-    element.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function(){
-   var interval = setInterval(moveRight, 10);
-  
-    
-};
-
-element.onclick =function(){
-    var interval1 = setInterval(moveRight, 10);
-};
+     counter = counter + 1;
+     var span = documnt.getElementById("count");
+     span.innerHTML = counter.toString();
+     
+     
+     
+ };
