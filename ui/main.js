79c0a4c7a7d6da47feb2af23304c14a27e297095
Siddhesh Kasar <ksiddhesh96.sk@gmail.com>
2017-08-23 00:57:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@
      //make a request to the counter endpoint
      
      
-     // Crate a request
+     // create a request
      var request = new XMLHttpRequest();
     
      
@@ -32,3 +32,46 @@
      
      
  };
+ 
+ //submit name
+ var nameInput = document.getElementById('name');
+ var name = nameInput.value;
+ var submit = document.getElementById('submit_btn');
+ submit.onclick = function(){
+//Make a request to the server and send the name
+
+
+
+//Capture the list of names and render it as a list.
+var names =['name1','name2','name3','name4'];
+var list ='';
+for(var i=0 ;i< names.length; i++){
+    
+    list += '<li>' + names[i] + '<li>' ;
+}
+ var ul = document.getElementById('namelist');
+ ul.innerHTML = list;
+ };
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
