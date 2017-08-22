c7bb436a006ce3a9a5cdb7e8bf3191cef39767e3
Siddhesh Kasar <ksiddhesh96.sk@gmail.com>
2017-08-23 00:27:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,5 @@
 //counter
- var button = document.getElementById("counter");
+ var button = document.getElementById('counter');
 
  button.onclick = function() {
      //make a request to the counter endpoint
@@ -16,7 +16,7 @@
        //take some action
        if(request.status === 200){
        var counter = request.responseText;
-        var span = document.getElementById("count");
+        var span = document.getElementById('count');
          span.innerHTML = counter.toString();
        }
        // not done
